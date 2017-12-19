a07321cf6913e4263ddf8987adeca9f819e23036
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2017-12-19 18:23:24 +1100

    Update decrypt.py

--- Python3/decrypt.py
+++ Python3/decrypt.py
@@ -1,17 +1,20 @@
 import string
 
-def decrypt():
-  initial = input('Give the message to encrypt (no space or special charcater) : ')
+def decrypt(initial):
+  """ Use : decrypt('zrffntr')
+  => 'message'
+  """
   initial = initial.lower()
-  numeros = []
+  list = []
   for char in initial:
-      numeros.append(ord(char) - 97)
+      list.append(ord(char) - 97)
   
   output = ""
   alphabet = dict(zip(range(0, 26), string.ascii_lowercase))
   
-  for char in numeros:
+  for char in list:
     if char in alphabet:
       image = (int(char) + 13) % 26
       output += alphabet[image]
-  print("Decrypted message is : " + output)
+  
+  return output
