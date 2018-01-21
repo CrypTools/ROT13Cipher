62a879bdc685b31dc49897af97007add53cb7b63
M0dEx <M0dEx@bitbucket.org>
2018-01-21 10:39:59 +0100

    Added C# ROT13 class.
    Signed-off-by: M0dEx <M0dEx@bitbucket.org>

---
+++ C#/ROT13.cs
@@ -0,0 +1,35 @@
+﻿/****************************************************/
+//
+//          Usage: ROT13.Use(String);
+//
+/****************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ROT13
+{
+    private readonly static char[] charSet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    public static string Use(string input)
+    {
+        input = input.ToUpper();
+
+        foreach(char c in input) 
+            if (!charSet.Contains(c))
+                return null;
+
+        string output = "";
+
+        foreach(char c in input)
+        {
+            int newPos = (charSet.ToList().IndexOf(c) + 13) % 26;
+            output += charSet[newPos];
+        }
+
+        return output;
+    }
+}
