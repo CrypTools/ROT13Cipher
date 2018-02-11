ef5a17bb6417751c54cfb3f3209de5a1203b8da9
Arthur Guiot <arguiot@gmail.com>
2018-02-11 19:38:58 +0800

    Fixed tests

--- __test__/test.js
+++ __test__/test.js
@@ -1,6 +1,6 @@
 // Test made using EyeJS - https://eye.js.org
 
-const path = require('path').normalize(__testDir + "/../JS/")
+const path = require('path').normalize(__testDir + "/../js/")
 
 const rot13 = require(path + "encrypt.js") // encrypt = decrypt.
 
