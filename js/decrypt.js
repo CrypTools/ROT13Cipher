708fdd2f9afe10c609e756a27ce461803bc13479
Arthur Guiot <arguiot@gmail.com>
2018-02-22 10:49:24 +0800

    Fixed JS code

--- js/decrypt.js
+++ js/decrypt.js
@@ -7,22 +7,22 @@
  * ========================================================================== */
 
 String.prototype.decrypt = function() {
-  output = ''
-  alphabet = {}
+    let output = ""
+    let alphabet = {}
 
-  chr = x => {return String.fromCharCode(x)}
+    const chr = x => String.fromCharCode(x)
 
-  for (var i = 0; i < 26; i++) {
-    alphabet[chr(65+i)] = chr(65 + (i + 13) % 26)
-    alphabet[chr(97+i)] = chr(97 + (i + 13) % 26)
-  }
+    for (let i = 0; i < 26; i++) {
+        alphabet[chr(65 + i)] = chr(65 + (i + 13) % 26)
+        alphabet[chr(97 + i)] = chr(97 + (i + 13) % 26)
+    }
 
-  for (var char of this) {
-    if (char in alphabet) output += alphabet[char]
-    else output += char
-  }
+    for (let char of this) {
+        if (char in alphabet) output += alphabet[char]
+        else output += char
+    }
 
-  return output
+    return output
 }
 
 module.exports = text => text.decrypt();
